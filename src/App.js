f1e43544220372fd2ea415fe062ea205bfc43751
Hi Tran <thuchitran@gmail.com>
2019-11-28 20:39:36 +0700

    first message

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Thuc Hi
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+// https://www.ninjavan.co/vi-vn/tracking
+// thanh nien
+// https://tinhte.vn/
+// https://www.airbnb.com.vn/
