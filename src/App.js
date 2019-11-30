85f13c59ceea8170a5a82d75a3488367444ef765
Hi Tran <thuchitran@gmail.com>
2019-11-30 17:01:59 +0700

    add data.jon and display product

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import product1 from './product1.jpg';
 import ProductInfo from './ProductInfo';
 import ProductImage from './ProductImage';
+import data from './data.json';
 import './App.css';
 
 function App() {
-  const product =
-  {
-    imgSrc: product1,
-    price: 240,
-    category: 'FURNITURE',
-    name: 'Minimal Deco Furniture',
-    isOnSale: true,
-    finalPrice: 100
-  };
+  const productList = data.data;
 
   return (
     <div className="App">
       <p>Hello ReactJS!</p>
       <p>Thuc Hi</p>
-        <div className="Product">
-          <ProductImage
-            isOnSale={product.isOnSale}
-            imgSrc={product.imgSrc} />
+      <div className="ProductList">
+        {productList.map(product =>
+          <div className="Product">
 
-          <ProductInfo
-            category={product.category}
-            name={product.name}
-            isOnSale={product.isOnSale}
-            finalPrice={product.finalPrice}
-            price={product.price} />
+            <ProductImage
+              isOnSale={parseInt(product.is_promotion) > 0}
+              imgSrc={product.img_url} />
+
+            <ProductInfo
+              category={product.category}
+              name={product.name}
+              isOnSale={parseInt(product.is_promotion) > 0}
+              finalPrice={product.final_price.toLocaleString()}
+              price={product.price.toLocaleString()} />
+          </div>
+        )}
       </div>
+
     </div>
   );
 }
