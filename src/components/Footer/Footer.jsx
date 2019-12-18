29df3b3891ff63d0dba01b8b4c4c7ed0c55d3a99
Hi Tran <thuchitran@gmail.com>
2019-12-19 05:54:09 +0700

    add Clock component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,91 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Clock from '../Clock/Clock';
 
 function Footer() {
-    const [time, setTime] = useState("");
-
-    const getTime = () => {
-        const today = new Date();
-        const hours = today.getHours();
-        const minutes = today.getMinutes();
-        const seconds = today.getSeconds();
-        const day = today.getDay();
-        const year = today.getFullYear();
-        const month = today.getMonth();
-        const date = today.getDate();
-        const time = `${getWeekDay(day)} ${date} ${getMonthName(month)}, ${year} ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} ${getSuffix(hours)}`;
-        setTime(time);
-    }
-
-    const getWeekDay = (number) => {
-        let weekDay = "";
-        switch (number) {
-            case 0:
-                weekDay = "Sun";
-            case 1:
-                weekDay = "Mon";
-            case 2:
-                weekDay = "Tue";
-            case 3:
-                weekDay = "Wed";
-            case 4:
-                weekDay = "Thu";
-            case 5:
-                weekDay = "Fri";
-            case 6:
-                weekDay = "Sat";
-        }
-        return weekDay;
-    }
-
-    const getMonthName = (number) => {
-        let month = "";
-        switch (number) {
-            case 0:
-                month = "Jan";
-            case 1:
-                month = "Feb";
-            case 2:
-                month = "Mar";
-            case 3:
-                month = "Apr";
-            case 4:
-                month = "May";
-            case 5:
-                month = "June";
-            case 6:
-                month = "Jul";
-            case 7:
-                month = "Aug";
-            case 8:
-                month = "Sep";
-            case 9:
-                month = "Oct";
-            case 10:
-                month = "Nov";
-            case 11:
-                month = "Dec";
-        }
-        return month;
-    }
-    const getSuffix = (hour) => {
-        if (hour < 12) {
-            return `AM`
-        } else {
-            return `PM`
-        }
-    }
-
-    const formatTime = (time) => {
-        if (time < 10) {
-            return `0${time}`;
-        } else {
-            return `${time}`
-        }
-    };
-
-    useEffect(() => {
-        setInterval(getTime, 1000);
-    }, [time])
     return (
         <footer className="footer-area pl-100 pr-100">
             <div className="footer-area box-90 pt-100 pb-60" data-background="img/bg/footer.jpg" style={{ "background-image": "url(&quot;img/bg/footer.jpg&quot)" }}>
@@ -96,13 +12,12 @@ function Footer() {
                                 <div className="footer-logo">
                                     <a href="#"><img src="./assets/logo_shop.png" alt="" /></a>
                                 </div>
-                                <h3>{time}</h3>
+                                <Clock/>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
                                     ut labore et dolore mag na
                                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                                     ex ea commodo consequat.
                                 </p>
-
                                 <div className="footer-time d-flex mt-30">
                                     <div className="time-icon">
                                         <img src="./assets/time.png" alt="" />
